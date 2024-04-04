c022582684cd7f2717f4e293817cc57eacdb78da
João Victor <victor.jvsb18@gmail.com>
2024-04-03 21:26:19 -0300

    implemented error-handler and cors configuration

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import fastify from "fastify";
 
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
 
 import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod"
 import { createEvent } from "./routes/create-event";
@@ -10,8 +11,13 @@ import { getEvent } from "./routes/get-event";
 import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import { errorHandler } from "./error-handler";
 
 export const app = fastify()
+
+app.register(fastifyCors, {
+  origin: '*',
+})
 
 app.register(fastifySwagger, {
   swagger: {
@@ -40,6 +46,8 @@ app.register(getAttendeeBadge);
 app.register(checkIn);
 app.register(getEventAttendees);
 
-app.listen({ port: 3333 }).then(() => {
+app.setErrorHandler(errorHandler)
+
+app.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
   console.log('HTTP server running!')
 })
